bc685204f5f616450e06899dbb608a8348d0dd8a
Aleksandr Samoylenko <ntskazhu1985@gmail.com>
2021-05-04 15:34:01 +0300

    Add task2

--- src/com/samoylenko/Main.java
+++ src/com/samoylenko/Main.java
@@ -1,8 +1,5 @@
 
-//        2) Реализовать метод который вернет строку String, в которой через пробел все числа от 1 до 99 со следующими изменениями:
-//        - если число кратно 3 то вывести вместо него Hello
-//        - если число кратно 5 то вывести вместо него World
-//        - если число кратно и 3 и 5 то вывести вместо него HelloWorld
+
 //        3*) Написать метод который ищет при помощи бинарного поиска есть ли число в отсортированном массиве. Входящие параметры: массив и число.
 //        (считаем что массива который нам передали отсортирован, проверять это не нужно)
 package com.samoylenko;
@@ -15,7 +12,8 @@ public class Main {
     public static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
-        task1();
+        //task1();
+        task2();
     }
 
     //        1) В банк положили S денег. Какой станет сумма вклада через N лет,
@@ -32,5 +30,26 @@ public class Main {
         }
         String finalResult =  new DecimalFormat("#0.00").format(depositAmount);
         System.out.println("For " + month + " month you'll get " + finalResult);
+    }
+
+//        2) Реализовать метод который вернет строку String, в которой
+//        через пробел все числа от 1 до 99 со следующими изменениями:
+//        - если число кратно 3 то вывести вместо него Hello
+//        - если число кратно 5 то вывести вместо него World
+//        - если число кратно и 3 и 5 то вывести вместо него HelloWorld
+    public static void task2() {
+        StringBuilder s = new StringBuilder();
+        for (int i = 1; i <= 99; i++) {
+            if ((i % 3) == 0 && (i % 5) == 0) {
+                s.append(" HelloWorld");
+            } else if (i % 3 == 0) {
+                s.append(" Hello");
+            }else if ((i % 5) == 0) {
+                s.append(" World");
+            } else
+            s.append(" " + i);
+
+        }
+        System.out.println(s);
     }
 }
